ed19176b649a03b902b8e5891c50c62eaa6f04c0
Jose Luis <wildchamo@gmail.com>
2024-12-14 12:37:20 -0500

    yaku title

--- src/components/ui/footer.jsx
+++ src/components/ui/footer.jsx
@@ -1,9 +1,15 @@
 import { UserInfoModal } from "@/hooks/user-info-modal";
+import { Button } from "./button";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-background border-t p-4 text-center">
+    <footer className="bg-background border-t p-4 px-10 flex justify-between">
       <UserInfoModal />
+
+      <Link href="/about">
+        <Button variant="outline">Acerca del proyecto</Button>
+      </Link>
     </footer>
   );
 }
