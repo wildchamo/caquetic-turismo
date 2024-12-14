f08cb6927507003949ab85873aa9a5ec9b43bbde
Yan Carlos Villegas <yancarlosvillegas7@gmail.com>
2024-12-14 16:00:56 -0500

    new change

---
+++ src/services/user/index.js
@@ -0,0 +1,27 @@
+import supabase  from './conection'; 
+
+export async function create_user({name, phone_number}) {
+  const { data, error } = await supabase
+    .from('users')
+    .insert({ name, phone_number });
+  if (error) {
+    console.error('Error al insertar datos:', error);
+  } else {
+    console.log('Datos insertados:', data);
+  }
+}
+
+export async function get_users() {
+  try {
+    const { data, error } = await supabase
+      .from('user')
+      .select('*');
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (err) {
+    console.error('Error al obtener datos:', err.message);
+    return null;
+  }
+}
