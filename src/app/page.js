513340b5caa762551b82324b90aadd15e7683b08
Jose Luis <wildchamo@gmail.com>
2024-12-15 03:28:47 -0500

    remove chat from main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 "use client";
 import { Map } from "@/components/map";
 
-import ChatBotAI from "../components/ui/chat";
 export default function Home() {
   return (
     <main>
       <Map />
       aún no sabes que destino tomar? hablar con nuestra ai para descrubirlo
-      <ChatBotAI />
     </main>
   );
 }
