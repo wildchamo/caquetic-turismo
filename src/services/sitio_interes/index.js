713812fc2342ee1539a7eac83865ad7e859a7f26
Yan Carlos Villegas <yancarlosvillegas7@gmail.com>
2024-12-15 18:31:35 -0500

    new commit

--- src/services/sitio_interes/index.js
+++ src/services/sitio_interes/index.js
@@ -15,6 +15,24 @@ export async function get_sitios() {
     }
 }
 
+export async function get_sitiosMunicipio(id) {
+    try {
+      const { data, error } = await supabase
+        .from('municipio')
+        .select('name, sitio_interes(*)')
+        .eq('id', id);
+      if (error) {
+        throw error;
+      }
+      console.log("asdasdasd")
+      console.log(data)
+      return data;
+    } catch (err) {
+      console.error('Error al obtener datos:', err.message);
+      return null;
+    }
+}
+
 export async function get_sitioById(id) {
   try {
     const { data, error } = await supabase
