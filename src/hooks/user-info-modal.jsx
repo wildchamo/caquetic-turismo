ebcf6d48c1eefb36d64bb67f417d4a90c2696cce
Jose Luis <wildchamo@gmail.com>
2024-12-16 04:17:07 -0500

    get points

--- src/hooks/user-info-modal.jsx
+++ src/hooks/user-info-modal.jsx
@@ -71,7 +71,8 @@ export function UserInfoModal({ type = "outline" }) {
     : "¡REGÍSTRATE!";
 
   const modalSubtitle = isLoggedIn
-    ? `${user.name} aquí puedes ver tus puntos`
+    ? `${user.name}, aquí puedes ver tus puntos y acceder a beneficios exclusivos. 🎉✨
+    - Consulta tus puntos acumulados. 🏆`
     : isLogin
     ? "Accede a tu cuenta para consultar los eventos y actividades en el Caquetá. 🦜🌿"
     : "Regístrate para recibir información exclusiva sobre eventos y actividades en el Caquetá. ¡Sé parte del club! 🌟";
@@ -87,145 +88,156 @@ export function UserInfoModal({ type = "outline" }) {
           <DialogTitle>{modalTitle}</DialogTitle>
           <DialogDescription>{modalSubtitle}</DialogDescription>
         </DialogHeader>
-        {isLogin ? (
-          <form onSubmit={handleSubmit(onSubmitLog)}>
-            <div className="grid gap-4">
-              {/* Campo para el correo */}
-              <div className="items-center gap-4">
-                <Label htmlFor="email" className="text-right">
-                  Correo Electrónico
-                </Label>
-                <Input
-                  id="email"
-                  {...register("email", {
-                    required: "El correo es obligatorio",
-                    pattern: {
-                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                      message: "Por favor, ingrese un correo válido",
-                    },
-                  })}
-                  className="col-span-3"
-                />
-                {errors.email && (
-                  <p className="col-span-3 col-start-2 text-sm text-red-500">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-            </div>
-
-            <DialogFooter style={{ marginTop: "1rem" }}>
-              <Button className="w-full" type="submit">
-                Iniciar Sesión
-              </Button>
-            </DialogFooter>
-          </form>
+        {isLoggedIn ? (
+          <h2 className=" mt-4">
+            Actualmente tienes{" "}
+            <span className="text-green-600">{user.points}</span> puntos. 🏆{" "}
+            ¡Sigue participando en nuestras actividades para ganar más
+            recompensas! 🌟
+          </h2>
         ) : (
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="grid gap-4">
-              {/* Campo para el nombre */}
-              <div className="items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Nombre
-                </Label>
-                <Input
-                  id="name"
-                  {...register("name", {
-                    required: "El nombre es obligatorio",
-                  })}
-                  className="col-span-3"
-                />
-                {errors.name && (
-                  <p className="col-span-3 col-start-2 text-sm text-red-500">
-                    {errors.name.message}
-                  </p>
-                )}
-              </div>
+          <>
+            {isLogin ? (
+              <form onSubmit={handleSubmit(onSubmitLog)}>
+                <div className="grid gap-4">
+                  {/* Campo para el correo */}
+                  <div className="items-center gap-4">
+                    <Label htmlFor="email" className="text-right">
+                      Correo Electrónico
+                    </Label>
+                    <Input
+                      id="email"
+                      {...register("email", {
+                        required: "El correo es obligatorio",
+                        pattern: {
+                          value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                          message: "Por favor, ingrese un correo válido",
+                        },
+                      })}
+                      className="col-span-3"
+                    />
+                    {errors.email && (
+                      <p className="col-span-3 col-start-2 text-sm text-red-500">
+                        {errors.email.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
 
-              {/* Campo para el teléfono */}
-              <div className="items-center gap-4">
-                <Label htmlFor="phone_number" className="text-right">
-                  Teléfono
-                </Label>
-                <Input
-                  id="phone_number"
-                  {...register("phone_number", {
-                    required: "El teléfono es obligatorio",
-                    pattern: {
-                      value: /^\+?[1-9]\d{1,14}$/,
-                      message:
-                        "Por favor, ingrese un número de teléfono válido",
-                    },
-                  })}
-                  className="col-span-3"
-                />
-                {errors.phone_number && (
-                  <p className="col-span-3 col-start-2 text-sm text-red-500">
-                    {errors.phone_number.message}
-                  </p>
-                )}
-              </div>
+                <DialogFooter style={{ marginTop: "1rem" }}>
+                  <Button className="w-full" type="submit">
+                    Iniciar Sesión
+                  </Button>
+                </DialogFooter>
+              </form>
+            ) : (
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid gap-4">
+                  {/* Campo para el nombre */}
+                  <div className="items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Nombre
+                    </Label>
+                    <Input
+                      id="name"
+                      {...register("name", {
+                        required: "El nombre es obligatorio",
+                      })}
+                      className="col-span-3"
+                    />
+                    {errors.name && (
+                      <p className="col-span-3 col-start-2 text-sm text-red-500">
+                        {errors.name.message}
+                      </p>
+                    )}
+                  </div>
 
-              {/* Campo para el correo */}
-              <div className="items-center gap-4">
-                <Label htmlFor="email" className="text-right">
-                  Correo Electrónico
-                </Label>
-                <Input
-                  id="email"
-                  {...register("email", {
-                    required: "El correo es obligatorio",
-                    pattern: {
-                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                      message: "Por favor, ingrese un correo válido",
-                    },
-                  })}
-                  className="col-span-3"
-                />
-                {errors.email && (
-                  <p className="col-span-3 col-start-2 text-sm text-red-500">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
+                  {/* Campo para el teléfono */}
+                  <div className="items-center gap-4">
+                    <Label htmlFor="phone_number" className="text-right">
+                      Teléfono
+                    </Label>
+                    <Input
+                      id="phone_number"
+                      {...register("phone_number", {
+                        required: "El teléfono es obligatorio",
+                        pattern: {
+                          value: /^\+?[1-9]\d{1,14}$/,
+                          message:
+                            "Por favor, ingrese un número de teléfono válido",
+                        },
+                      })}
+                      className="col-span-3"
+                    />
+                    {errors.phone_number && (
+                      <p className="col-span-3 col-start-2 text-sm text-red-500">
+                        {errors.phone_number.message}
+                      </p>
+                    )}
+                  </div>
 
-              <div className="items-center gap-4 mt-4">
-                <Label htmlFor="password" className="text-right">
-                  Contraseña
-                </Label>
-                <Input
-                  id="password"
-                  type="password"
-                  {...register("password", {
-                    required: "La contraseña es obligatoria",
-                  })}
-                  className="col-span-3"
-                />
-                {errors.password && (
-                  <p className="col-span-3 col-start-2 text-sm text-red-500">
-                    {errors.password.message}
-                  </p>
-                )}
-              </div>
-            </div>
+                  {/* Campo para el correo */}
+                  <div className="items-center gap-4">
+                    <Label htmlFor="email" className="text-right">
+                      Correo Electrónico
+                    </Label>
+                    <Input
+                      id="email"
+                      {...register("email", {
+                        required: "El correo es obligatorio",
+                        pattern: {
+                          value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                          message: "Por favor, ingrese un correo válido",
+                        },
+                      })}
+                      className="col-span-3"
+                    />
+                    {errors.email && (
+                      <p className="col-span-3 col-start-2 text-sm text-red-500">
+                        {errors.email.message}
+                      </p>
+                    )}
+                  </div>
 
-            <DialogFooter style={{ marginTop: "1rem" }}>
-              <Button className="w-full" type="submit">
-                Registrarse
-              </Button>
-            </DialogFooter>
-          </form>
+                  <div className="items-center gap-4 mt-4">
+                    <Label htmlFor="password" className="text-right">
+                      Contraseña
+                    </Label>
+                    <Input
+                      id="password"
+                      type="password"
+                      {...register("password", {
+                        required: "La contraseña es obligatoria",
+                      })}
+                      className="col-span-3"
+                    />
+                    {errors.password && (
+                      <p className="col-span-3 col-start-2 text-sm text-red-500">
+                        {errors.password.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+
+                <DialogFooter style={{ marginTop: "1rem" }}>
+                  <Button className="w-full" type="submit">
+                    Registrarse
+                  </Button>
+                </DialogFooter>
+              </form>
+            )}
+            {/* Botón para alternar entre login y registro */}
+            <h4
+              onClick={() => setIsLogin(!isLogin)}
+              style={{ cursor: "pointer" }}
+              className="text-indigo-600 text-center mt-4 hover:text-indigo-800 hover:underline transition duration-200 ease-in-out"
+            >
+              {isLogin
+                ? "¿Eres nuevo aquí? Regístrate para unirte al club🦜🌿"
+                : "¿Ya tienes cuenta🙆‍♂️? Inicia sesión"}
+            </h4>
+          </>
         )}
-        {/* Botón para alternar entre login y registro */}
-        <h4
-          onClick={() => setIsLogin(!isLogin)}
-          style={{ cursor: "pointer" }}
-          className="text-indigo-600 text-center mt-4 hover:text-indigo-800 hover:underline transition duration-200 ease-in-out"
-        >
-          {isLogin
-            ? "¿Eres nuevo aquí? Regístrate para unirte al club🦜🌿"
-            : "¿Ya tienes cuenta🙆‍♂️? Inicia sesión"}
-        </h4>
       </DialogContent>
     </Dialog>
   );
